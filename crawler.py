170fe18be1005ddaad0733ba0601774b96866ae4
Fellita Candini <fellitacandini@gmail.com>
2017-12-10 06:01:39 +0700

    badhanshake in the mid

--- crawler.py
+++ crawler.py
@@ -21,26 +21,28 @@ class Crawler:
 
     def make_corpus(self):
         stream="0"
-        #file = open('corpus.txt','w') #write in a file
+        file = open('corpus.txt','w') #write in a file
         for cat in range(len(self.id_kategori)):
             while True:
-                url_stream = 'https://lapor.go.id/home/streams/{}/{}/old/beranda'.format(stream,id_kategori[cat])
+                url_stream = 'https://lapor.go.id/home/streams/{}/{}/old/beranda'.format(stream,self.id_kategori[cat])
                 raw = requests.get(url_stream).text
                 data = json.loads(raw)
                 if len(data) == 0:
                     stream="0"
                     break
                 for d in range(len(data)):
-                    doc = Document(data[d]['nid'],data[d]['subject'],data[d]['content']) #making object
-                    self.document.append(doc)
-                    #file.write("{}\n".format(data[d]['nid']))
-                    #file.write("{}\n".format(data[d]['subject']))
-                    #file.write("{}\n\n".format(data[d]['content']))
+                    #doc = Document(data[d]['nid'],data[d]['subject'],data[d]['content']) #making object
+                    #self.document.append(doc)
+                    file.write("{}\n".format(data[d]['nid']))
+                    file.write("{}\n".format(data[d]['subject']))
+                    file.write("{}\n\n".format(data[d]['content']))
                 stream=data[len(data)-1]['last_activity']
-                #print(stream) #check berhenti sampai mana
-        #file.close()
+                print(stream) #check berhenti sampai mana
+        file.close()
 
     def get_document(self):
         return self.document
 
-#gather_id_category(url)
+c = Crawler()
+c.gather_id_category()
+c.make_corpus()

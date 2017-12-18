70bf460a39c35d4cce6ee154fe91162b343bb4db
Fellita Candini <fellitacandini@gmail.com>
2017-12-19 03:03:40 +0700

    frontend

--- index.js
+++ index.js
@@ -17,22 +17,33 @@
 
       .cloud {
         display: inline-block;
+        background-color: blue;
+        margin-left: 1em;
+        margin-right:1em;
+        margin-top: 1em;
+        margib-bottom: 1em;
+        -webkit-transition: 0.3s box-shadow ease;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+        border-radius: 5px;
+        background-color: #e6eaed;
       }
 
-      .left {
-        margin-left: auto;
+      .container {
+        margin: 0 auto;
+        max-width: 112.0rem;
+        padding: auto;
+        position: relative;
+        width: 100%;
       }
 
-      .right {
-        margin-right: auto;
-      }
+
     </style>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
     <script type="text/javascript" src="jqcloud-1.0.4.js"></script>
     <script type="text/javascript">
     $(document).ready(function() {
       var word_array = [];
-        
+
       $.ajax({
           type: "GET",
           url: "output.csv",
@@ -77,25 +88,27 @@
     </script>
   </head>
   <body>
-    <h1>Lapor Classifier</h1>
-    <p>Lapor Classifier is a data visualization webpage providing
-      insights on lapor.go.id topic distribution.
-        Since lapor.go.id do not have tags feature, 
-        this application purpose is to create relevant tags and to visualize 
-        the big picture of all lapor.go.id entries. 
-        Using Latent Dirichlet Allocation (LDA) for the topic modelling. Built with Python &amp; jQCloud.
-        <br/>Created as the fourth assignment of Information Retrieval course (IKO41357).
-    </p>
+    <div class="container">
+      <h1>Lapor Classifier</h1>
+      <p>Lapor Classifier is a data visualization webpage providing
+        insights on lapor.go.id topic distribution.
+          Since lapor.go.id do not have tags feature,
+          this application purpose is to create relevant tags and to visualize
+          the big picture of all lapor.go.id entries.
+          Using Latent Dirichlet Allocation (LDA) for the topic modelling. Built with Python &amp; jQCloud.
+          <br/>Created as the fourth assignment of Information Retrieval course (IKO41357).
+      </p>
 
-    <div id="wordcloud1" class="cloud left" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud2" class="cloud right" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud3" class="cloud left" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud4" class="cloud right" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud5" class="cloud left" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud6" class="cloud right" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud7" class="cloud left" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud8" class="cloud right" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud9" class="cloud left" style="width: 550px; height: 350px;"></div>
-    <div id="wordcloud10" class="cloud right" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud1" class="cloud left" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud2" class="cloud right" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud3" class="cloud left" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud4" class="cloud right" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud5" class="cloud left" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud6" class="cloud right" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud7" class="cloud left" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud8" class="cloud right" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud9" class="cloud left" style="width: 550px; height: 350px;"></div>
+      <div id="wordcloud10" class="cloud right" style="width: 550px; height: 350px;"></div>
+    </div>
   </body>
 </html>
